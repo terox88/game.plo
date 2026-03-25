b48bddbbbc6eddd3748132479c64ce214f7f457f
Michal <terox@wp.pl>
2026-03-25 23:24:57 +0100

    init project

---
+++ backend/src/main/java/com/game/plo/PloApplication.java
@@ -0,0 +1,13 @@
+package com.game.plo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PloApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/game/plo/PloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.game.plo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
